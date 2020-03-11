9cb2a4d6931573028ad4a17b5c31b1bdc54c414c
BaeWooRam <41356481+BaeWooRam@users.noreply.github.com>
2020-03-11 16:35:37 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/coroutinetest/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetest/MainActivity.kt
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     val service =
         ServiceBuilder.getService("https://jsonplaceholder.typicode.com/", UserService::class.java)
+
+    var _userList: List<User>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +18,55 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         GlobalScope.launch(Dispatchers.IO) {
             val job = service.getUsers()
 
-            delegateUser(job).collect{
+//                        delegateUser(job).flowOn() Context수정
+
+            //필터 & Collect
+            delegateUser(job)
+                .filter {
+                    return@filter it.id == 1
+                }.collect {
+                    Log.i(javaClass.simpleName, it.toString())
+                }
+
+//            Map & Collect
+            delegateUser(job).map {
+                it.email
+            }.collect{
                 Log.i(javaClass.simpleName, it.toString())
             }
+
+//            필터 & toList
+            _userList = delegateUser(job).filter {
+                return@filter true
+            }.toList()
+
+            Log.i("필터 & toList", "size : ${_userList?.size}")
+
+            //Take & Reduce
+            val user = delegateUser(job)
+                .reduce { accumulator, value ->
+                    accumulator.id = accumulator.id + value.id
+                    Log.i("Take & Reduce", "acc : ${accumulator.toString()}")
+                    Log.i("Take & Reduce", "value : ${value.toString()}")
+                    return@reduce accumulator
+                }
+
+            Log.i(javaClass.simpleName, user.toString())
+
+            // Zip
+            delegateUser(job)
+                .zip(flowOf(10, 20, 30, 40, 50)) { user, num ->
+                user.id = num
+                    return@zip user
+                }.collect {
+                Log.i("Zip USER", it.toString())
+                }
+
         }
 
     }
 
-    private fun delegateUser(users: List<User>):Flow<User> = flow{
+    private fun delegateUser(users: List<User>): Flow<User> = flow {
         Log.i(javaClass.simpleName, "isEmpty : ${users.isEmpty()}")
 
         for (user in users) {
